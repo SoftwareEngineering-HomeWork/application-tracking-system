4a62da265511d7fd6b7675e3da20e20f81d09223
Prithish Samanta <prithishsamanta@gmail.com>
2024-10-22 23:15:10 -0400

    Add Files

---
+++ dashboard/src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Sidebar.css';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-item">Search</div>
+      <div className="sidebar-item">Manage</div>
+      <div className="sidebar-item">Matches</div>
+      <div className="sidebar-item">Applications</div>
+      <div className="sidebar-item">Profile</div>
+      <div className="sidebar-item">Logout</div>
+    </div>
+  );
+}
+
+export default Sidebar;
