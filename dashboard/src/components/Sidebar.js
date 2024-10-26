134e64b109ec3be939eb564e06ff5747cba1034b
Prithish Samanta <prithishsamanta@gmail.com>
2024-10-25 22:17:20 -0400

    add candidate page

--- dashboard/src/components/Sidebar.js
+++ dashboard/src/components/Sidebar.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './components.css';
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sidebar-item">Profile</div>
-      <div className="sidebar-item">Matches</div>
+      <Link to="/" className="sidebar-item">Profile</Link>
+      <Link to="/matches" className="sidebar-item">Matches</Link>
       <div className="sidebar-item">Logout</div>
     </div>
   );
